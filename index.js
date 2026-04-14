fc38a23108aa6dbfba5d4badd0fa99a05265f3bc
222112417 <222112417@stis.ac.id>
2026-04-14 09:01:35 +0700

    changes ip address

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
 
 app.post('/generate-text', async (req, res) => {
